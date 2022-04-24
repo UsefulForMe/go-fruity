23a815eae9b0fa93a5842185079350bae8c38786
trongdev <hiiamtrong@gmail.com>
2022-04-24 18:56:56 +0700

    add transaction create user address

--- repository/user_address.go
+++ repository/user_address.go
@@ -25,15 +25,34 @@ func NewUserAddressRepository(db *gorm.DB) DefaultUserAddressRepository {
 }
 
 func (r DefaultUserAddressRepository) Save(userAddress models.UserAddress) (*models.UserAddress, *errs.AppError) {
+
+	tx := r.db.Begin()
+
+	if userAddress.IsDefault {
+		if err := r.db.Model(&models.UserAddress{}).Where("user_id = ?", userAddress.UserID).Update("is_default", false).Error; err != nil {
+			logger.Error("Error while updating user addresses " + err.Error())
+			tx.Rollback()
+			return nil, errs.NewUnexpectedError("Error while updating user addresses " + err.Error())
+		}
+	}
 	if err := r.db.Create(&userAddress).Error; err != nil {
 		logger.Error("Error while saving user address " + err.Error())
+		tx.Rollback()
 		return nil, errs.NewUnexpectedError("Error while saving user address " + err.Error())
 	}
+
+	if err := tx.Commit().Error; err != nil {
+		logger.Error("Error while saving user address " + err.Error())
+		tx.Rollback()
+		return nil, errs.NewUnexpectedError("Error while saving user address " + err.Error())
+	}
+
 	return &userAddress, nil
 }
 
 func (r DefaultUserAddressRepository) FindUserAddressesByUserID(userID uuid.UUID) ([]models.UserAddress, *errs.AppError) {
 	var userAddresses []models.UserAddress
+
 	if err := r.db.Where("user_id = ?", userID).Find(&userAddresses).Error; err != nil {
 		logger.Error("Error while finding user addresses " + err.Error())
 		return nil, errs.NewUnexpectedError("Error while finding user addresses " + err.Error())
