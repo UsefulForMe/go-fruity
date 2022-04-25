692bcfb88c67f0d7152e3af426dfcc8a04b513d0
trongdev <hiiamtrong@gmail.com>
2022-04-26 02:30:59 +0700

    fix user address

--- dto/user_address.go
+++ dto/user_address.go
@@ -10,7 +10,7 @@ type CreateUserAddressRequest struct {
 	IsDefault   bool      `json:"is_default" gorm:"default:false"`
 	PhoneNumber string    `json:"phone_number" gorm:"type:varchar(100);not null"`
 	FullName    string    `json:"full_name" gorm:"type:varchar(100)"`
-	Address     string    `json:"address" gorm:"type:varchar(100); not null"`
+	Address     string    `json:"address" gorm:"type:varchar(1000); not null"`
 	Longitude   float32   `json:"longitude" gorm:"type:numeric; not null"`
 	Latitude    float32   `json:"latitude" gorm:"type:numeric; not null"`
 	Note        string    `json:"note" gorm:"type:varchar(255);"`
